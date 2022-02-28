70285a52c045606ab9ee88bb23e118f8c031bf8c
chloepomeroy <pomeroyc@outlook.com>
2022-02-28 23:06:42 +0000

    partway through 4

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -85,13 +85,15 @@ def registration_request(request):
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     if request.method == "GET":
+        context = {}
         url = "https://7dc9cbcd.us-south.apigw.appdomain.cloud/capstone/get_dealerships"
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
         # Concat all dealer's short name
         dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
         # Return a list of dealer short name
-        return HttpResponse(dealer_names)
+        context["dealerships"] = dealerships
+        return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
