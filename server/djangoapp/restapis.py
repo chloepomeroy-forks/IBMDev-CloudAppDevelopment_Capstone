97e258284c5dfca2c8e29405fdc0af74edb91154
chloepomeroy <68558959+chloepomeroy@users.noreply.github.com>
2022-03-09 12:52:31 -0500

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -98,9 +98,9 @@ def analyze_review_sentiments(dealerreview):
     natural_language_understanding = NaturalLanguageUnderstandingV1(version='2021-08-01',authenticator=authenticator)
     natural_language_understanding.set_service_url(url)
     try:
-        response = natural_language_understanding.analyze( text=dealerreview,features=Features(sentiment=SentimentOptions(targets=[dealerreview]))).get_result()
+        response = natural_language_understanding.analyze( text=dealerreview,features=Features(sentiment=SentimentOptions())).get_result()
         print(response)
-        label=json.dumps(response, indent=2)["sentiment"]["targets"]["label"]
+        label=response["sentiment"]["targets"]["label"]
         return(label)
     except:
         return("neutral")
